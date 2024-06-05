c0d27ace2d242f9049a1eab06794569a742d48fb
KIMBERLY_TAPFUMANEYI_KIMBERLY <kimberlytapfumanyi16@gmail.com>
2024-06-05 20:36:18 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,1 +1,31 @@
-n
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import RecipesListPage from './pages/RecipesListPage';
+import RecipeDetailPage from './pages/RecipeDetailPage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About Us</Link></li>
+            <li><Link to="/contact">Contact Us</Link></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={RecipesListPage} />
+          <Route path="/recipe/:id" component={RecipeDetailPage} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/contact" component={ContactPage} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
