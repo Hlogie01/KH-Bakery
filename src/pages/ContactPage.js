8aa7b0cf4bba471a33d3ed382576f986f7c534d8
Hlogie01 <135062998+Hlogie01@users.noreply.github.com>
2024-06-05 22:56:52 +0200

    contact page update

---
+++ src/pages/ContactPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContactPage = () => {
+  return (
+    <div className="contact-page">
+      <h1>Contact Us</h1>
+      <p>If you have any questions, feel free to reach out to us at contact@KHrecipebook.com.</p>
+    </div>
+  );
+};
+
+export default ContactPage;
